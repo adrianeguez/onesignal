65366453e84c4b1caa3bed9f54384aba79931256
Adrian Eguez <eadepto@hotmail.com>
2019-01-20 12:37:51 -0500

    proyecto de android

--- one-signal/src/app/app.component.ts
+++ one-signal/src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component} from '@angular/core';
+import {Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import {TabsPage} from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +17,23 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      // OneSignal Code start:
+      // Enable to debug issues:
+      /*
+      window["plugins"].OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+      const notificationOpenedCallback = (jsonData)=> {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+      };
+
+
+      window["plugins"].OneSignal
+        .startInit("bb8c0b30-38d4-4a9b-a961-8c88e1ca6896", "508911833017")
+        .handleNotificationOpened(notificationOpenedCallback)
+        .endInit();
+        */
+
     });
   }
 }
